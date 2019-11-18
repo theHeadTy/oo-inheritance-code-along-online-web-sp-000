6ed259adf8b2a97b2003f9eddf58a95a2feeceb6
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 19:18:58 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,17 @@
 class Vehicle
-
+  
+  attr_accessor :wheel_size, :wheel_number
+  
+  def initialize(size, number)
+    @wheel_size = size
+    @wheel_number = number
+  end
+  
+  def go
+    'vrrrrrrrooom!'
+  end
+  
+  def fill_up_tank
+    'filling up!'
+  end
 end
